93253980d2015b0e70fce328147d6ef05fc81850
Jake Vossen <jakvossen5@gmail.com>
2017-07-22 00:52:59 -0600

    Cleaned code

--- js/genQuestions.js
+++ js/genQuestions.js
@@ -3,6 +3,9 @@ questions = { "questions":[
   {"question":"What were the first words spoken on the moon?", 
   "answers":[ "...one small step for a man, one giant leap for mankind", "The pledge of allegiance", "The preamble of the constitution", "Not known, the radio data failed"], 
   "explanation":"\"...one small step for a man, one giant leap for mankind\" were the first words on the moon, spoken my Neil Armstrong. He claims he siad \"...one small step for a man, AND ONE giant leap for mankind\", but the radio did not pick up on it.", "source":"https://www.nasa.gov/mission_pages/apollo/missions/apollo11.html" }]};
+
+
+//finalize
 myJSON = JSON.stringify(questions);
 localStorage.setItem("questions", myJSON);
 console.log("wrote data");
