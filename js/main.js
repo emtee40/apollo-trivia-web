d16078287554e31d7c0d991e8230e065fe910396
Jake Vossen <jakvossen5@gmail.com>
2017-07-22 23:20:46 -0600

    Random answers now

--- js/main.js
+++ js/main.js
@@ -17,6 +17,31 @@ function submitValues() {
 }
 
 function setQuestions() {
-  console.log("setting questions")
-  document.querySelector('#answer0id ~ label').innerHTML = 'testing123'; 
+  curCorrectAnswer = curQuestion.answers[0]
+  answersRandomized = shuffle(curQuestion.answers)
+  document.querySelector('#question').innerHTML = curQuestion.question;
+  document.querySelector('#answer0id ~ label').innerHTML = curQuestion.answers[0]; 
+  document.querySelector('#answer1id ~ label').innerHTML = curQuestion.answers[1];
+  document.querySelector('#answer2id ~ label').innerHTML = curQuestion.answers[2];
+  document.querySelector('#answer3id ~ label').innerHTML = curQuestion.answers[3];
+}
+
+//stolen from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
